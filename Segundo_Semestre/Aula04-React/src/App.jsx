7e459b5605a90f018bfa3cbd9f5375f76bc27b44
RodrigoBatis <rodrigofreirebatista@gmail.com>
2023-08-29 22:56:35 -0300

    Aula04 inicio React

---
+++ Segundo_Semestre/Aula04-React/src/App.jsx
@@ -0,0 +1,31 @@
+import ListaAlunos from "./Components/listaAlunos"
+
+export default function App(){
+
+  const frutas = 'Banana'
+  const semana = 'Terça-Feira'
+
+  return(//Retorna todo o conteudo que será exibido na tela
+    
+    <>
+      <h1>sasasasas</h1>
+      <hr />
+      <ListaAlunos />
+      <hr />
+
+      <p>dadfggfdf</p>
+      <ul>
+        <li><a href="">x</a></li>
+        <li><a href="">t</a></li>
+        <li><a href="">u</a></li>
+        <li><a href="">v</a></li>
+      </ul>
+      <br />
+      <hr />
+      <p>{frutas}</p>
+      <h1>{semana}</h1>
+
+    </>
+
+  )
+}
---
+++ Segundo_Semestre/Aula04-React/src/Components/ListaAlunos.jsx
@@ -0,0 +1,13 @@
+export default function ListaAlunos(){
+
+   return(
+      <ul>
+         <li>Bruno</li>
+         <li>Pedro</li>
+         <li>Tadeu</li>
+         <li>Irineu</li>
+         <li>Jubileu</li>
+      </ul>
+   )
+
+}
