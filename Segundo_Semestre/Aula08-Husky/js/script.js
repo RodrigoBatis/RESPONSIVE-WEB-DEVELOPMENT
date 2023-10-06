ba76570eaa7d079712ff14fd3b19507bf410c8b6
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-05 22:41:02 -0300

    Site husky menu burguer

---
+++ Segundo_Semestre/Aula08-Husky/js/script.js
@@ -0,0 +1,9 @@
+function btnMenu(){
+    var menu = document.getElementsByClassName('menu');
+    menu[0].style.display = 'block';
+}
+
+function btnMenu2(){
+    var menu = document.getElementsByClassName('menu');
+    menu[0].style.display='none'
+}
